3b9e3bf816d99dd22506dbad309b4e0b2bde92e6
aalampatil <aalam.workacc@gmail.com>
2026-03-27 15:39:10 +0530

    some fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,7 @@ import Homepage from './pages/Homepage'
 import GenerateLink from './pages/GenerateLink'
 import PublicUrl from './pages/PublicUrl'
 import ManageUrlPage from './pages/ManageUrlPage'
+import { ClerkProvider } from '@clerk/react'
 
 const router = createBrowserRouter(
   [
@@ -37,6 +38,8 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY} >
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>,
 )
