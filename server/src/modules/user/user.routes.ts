f2dc8855306ff53bf9f85a9424577f9b77099d84
aalampatil <aalam.workacc@gmail.com>
2026-03-28 13:08:49 +0530

    fix 5

--- server/src/modules/user/user.routes.ts
+++ server/src/modules/user/user.routes.ts
@@ -1,18 +1,18 @@
-import { Router } from "express";
-import { clerkClient, requireAuth, getAuth } from "@clerk/express";
-import type { Request, Response } from "express";
+// import { Router } from "express";
+// import { clerkClient, requireAuth, getAuth } from "@clerk/express";
+// import type { Request, Response } from "express";
 
-export const userRouter = Router();
+// export const userRouter = Router();
 
-userRouter
-  .route("/profile")
-  .get(requireAuth(), async (req: Request, res: Response) => {
-    const { userId } = getAuth(req);
-    console.log(userId);
-    if (!userId) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
+// userRouter
+//   .route("/profile")
+//   .get(requireAuth(), async (req: Request, res: Response) => {
+//     const { userId } = getAuth(req);
+//     console.log(userId);
+//     if (!userId) {
+//       return res.status(401).json({ error: "Unauthorized" });
+//     }
 
-    const user = await clerkClient.users.getUser(userId);
-    return res.json({ user });
-  });
+//     const user = await clerkClient.users.getUser(userId);
+//     return res.json({ user });
+//   });
