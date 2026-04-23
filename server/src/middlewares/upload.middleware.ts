d1e09a2b145f650baf70c1c539e1faf08a9ebb0e
aalampatil <aalam.workacc@gmail.com>
2026-04-24 01:21:48 +0530

    multer file filter and limit

--- server/src/middlewares/upload.middleware.ts
+++ server/src/middlewares/upload.middleware.ts
@@ -1,6 +1,19 @@
 import multer, { type StorageEngine } from "multer";
 import { type Request } from "express";
 
+// Allowed MIME types
+const allowedMimeTypes = [
+  // Images
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+
+  // Videos
+  "video/mp4",
+  "video/mpeg",
+  "video/quicktime", // .mov
+];
+
 const storage: StorageEngine = multer.diskStorage({
   destination: (
     req: Request,
@@ -19,6 +32,22 @@ const storage: StorageEngine = multer.diskStorage({
   },
 });
 
-export const upload = multer({ storage });
+const fileFilter: multer.Options["fileFilter"] = (req, file, cb) => {
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type. Only images and videos are allowed."));
+  }
+};
+
+const limits = {
+  fileSize: 50 * 1024 * 1024, // 50MB
+};
+
+export const upload = multer({
+  storage,
+  fileFilter,
+  limits,
+});
 
 // todo - add file filter and validation
